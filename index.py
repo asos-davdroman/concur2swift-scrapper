f811a70f109f063c43f5ffee8469216593585854
asos-davdroman <david.roman@asos.com>
2018-06-14 16:14:02 +0100

    Gardening

--- index.py
+++ index.py
@@ -9,18 +9,17 @@ soup = scrapper(page.content, 'html.parser')
 raw_models = soup.find_all('div', class_='set-description')
 
 for raw_model in raw_models:
-    raw_model_name = raw_model.find('h2')
-
     # Only allow actual models
+    raw_model_name = raw_model.find('h2')
     if 'id="type-' not in str(raw_model_name):
         continue
 
-    raw_model_tables = raw_model.find_all('table')
-
     # Only allow models with tables (properties)
+    raw_model_tables = raw_model.find_all('table')
     if not raw_model_tables:
         continue
 
+    # Extract model name and description
     model_name = raw_model.find('h2', recursive=False).get_text()
     try:
         model_descriptions = [x.get_text() for x in raw_model.find_all('p', recursive=False)]
